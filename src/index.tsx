c72f5f013fe5368b1f9a3c7408adfbb5af0e18aa
Valentino Janjac <valentino.janjac@ailend.io>
2022-11-14 16:36:03 +0100

    I think im done

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.scss';
 import reportWebVitals from './reportWebVitals';
+import AppRouter from './AppRouter';
+import { NotesProvider } from './context/NotesContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <NotesProvider>
+    <AppRouter />
+    </NotesProvider>
   </React.StrictMode>
 );
 
